5011a3b1aa488815dda10beac1912d879c897d9d
jeyaprabu <jeyaprabuj@gmail.com>
2015-08-07 15:48:07 +0530

    Revert "Exploratory Data Analysis"
    This reverts commit f1764b8e3b886026e91a0a5c20227cca04609678.

--- plot1.R
+++
@@ -1,9 +0,0 @@
-plot1<-function(){
-      
-      hh <- read.csv("G:/Online/Coursera/Data Science Specialisation/Exploratory Data Analysis/pro/cp1/exdata-data-household_power_consumption/household_power_consumption.txt", sep=";",na.strings = "?")
-      hh$Date<-as.Date(hh$Date,format="%d/%m/%Y")
-      t<-hh[which(hh$Date %in% as.Date(c("2007-02-02","2007-02-01"))),]
-      hist(as.numeric(t$Global_active_power),main="Global Active Power",col="orange",xlab="Global Active Power(kilowatts)")
-      dev.copy(png,file="plot1.png")
-      dev.off()
-}
